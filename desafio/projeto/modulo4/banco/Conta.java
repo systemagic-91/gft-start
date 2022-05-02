b41dc49e00a70a6a1d5a2993c8078b226f853d41
systemagic-91 <rayanepaiiva@live.com>
2022-05-01 23:53:02 -0300

    composição conta cliente

--- desafio/projeto/modulo4/banco/Conta.java
+++ desafio/projeto/modulo4/banco/Conta.java
@@ -5,13 +5,15 @@ public abstract class Conta implements IConta{
     protected int agencia;
     protected int numero;
     protected double saldo;
+    protected Cliente cliente;
 
     private static final int AGENCIA_PADRAO = 1;
     private static int SEQUENCIAL = 1;
 
-    public Conta(){
+    public Conta(Cliente cliente){
         this.agencia = AGENCIA_PADRAO;
         this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
     }
 
     public int getAgencia(){
