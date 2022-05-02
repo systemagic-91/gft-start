6453f9c10fff6e252770de4ab44a3bd3f4304a5b
systemagic-91 <rayanepaiiva@live.com>
2022-05-01 23:54:04 -0300

    add contstrutores

--- desafio/projeto/modulo4/banco/ContaCorrente.java
+++ desafio/projeto/modulo4/banco/ContaCorrente.java
@@ -2,6 +2,11 @@ package desafio.projeto.modulo4.banco;
 
 public class ContaCorrente extends Conta{
 
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+        //TODO Auto-generated constructor stub
+    }
+
     @Override
     public void extrato(){
         System.out.println("=== EXTRATO CONTA CORRENTE ===");
--- desafio/projeto/modulo4/banco/ContaPoupanca.java
+++ desafio/projeto/modulo4/banco/ContaPoupanca.java
@@ -1,6 +1,11 @@
 package desafio.projeto.modulo4.banco;
 
 public class ContaPoupanca extends Conta{
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+        //TODO Auto-generated constructor stub
+    }
+
     @Override
     public void extrato(){
         System.out.println("=== EXTRATO CONTA POUPANÇA ===");
