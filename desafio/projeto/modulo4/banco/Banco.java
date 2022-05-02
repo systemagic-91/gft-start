f08505c55e967c7d2114f9988d51682ffc615a2d
systemagic-91 <rayanepaiiva@live.com>
2022-05-01 23:36:26 -0300

    classes banco definida

---
+++ desafio/projeto/modulo4/banco/Banco.java
@@ -0,0 +1,13 @@
+package desafio.projeto.modulo4.banco;
+
+public class Banco {
+    private String nome;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+}
