764f38477d583a0bb00bdf1b5195dfde28891506
systemagic-91 <rayanepaiiva@live.com>
2022-05-02 00:14:05 -0300

    create constructor

--- desafio/projeto/modulo4/banco/Cliente.java
+++ desafio/projeto/modulo4/banco/Cliente.java
@@ -1,7 +1,17 @@
 package desafio.projeto.modulo4.banco;
 
 public class Cliente {
-    public Cliente(){
-        
+
+    public String nome;
+    public String profissao;
+    public Endereco endereco;
+    public Telefone telefone;
+
+
+    public Cliente(String nome, String profissao, Endereco endereco, Telefone telefone){
+        this.nome = nome;
+        this.profissao = profissao;
+        this.endereco = endereco;
+        this.telefone = telefone;
     }
 }
