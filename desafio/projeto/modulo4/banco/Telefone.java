221566ce4c0b534b4d059181158bbb10684b9f31
systemagic-91 <rayanepaiiva@live.com>
2022-05-02 00:08:11 -0300

    create class phone number

---
+++ desafio/projeto/modulo4/banco/Telefone.java
@@ -0,0 +1,31 @@
+package desafio.projeto.modulo4.banco;
+
+public class Telefone {
+    public int ddi;
+    public int ddd;
+    public int numero;
+
+    public int getDdi() {
+        return ddi;
+    }
+
+    public int getDdd() {
+        return ddd;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setDdi(int ddi) {
+        this.ddi = ddi;
+    }
+
+    public void setDdd(int ddd) {
+        this.ddd = ddd;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+}
